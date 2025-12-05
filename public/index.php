96f817b1deaa9793c00b5cab79a67b7e805ecc09
peyman <peymandev01@gmail.com>
2025-12-05 20:38:11 +0330

    add initial project files

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="./styles/app.css" rel="stylesheet">
+</head>
+<body class="flex h-screen items-center justify-center">
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</body>
+</html>
