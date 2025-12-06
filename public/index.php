92e81be79142f8c444bab3b170d2fd0fea55e7a7
peyman <peymandev01@gmail.com>
2025-12-07 01:53:26 +0330

    create nav bar

--- public/index.php
+++ public/index.php
@@ -5,10 +5,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="./styles/app.css" rel="stylesheet">
 </head>
-<body class="flex h-screen items-center justify-center">
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus cum, molestias ipsa dignissimos a eos eaque? Ea ipsam aspernatur numquam nam incidunt vitae distinctio ab molestiae dolorum! Iste, officia eos?</p>
+<body class="font-Vazir">
+    <nav class="bg-primary h-[70px] flex items-center">
+      <div class="container">
+        <h3 class="text-white font-bold text-3xl">بلاگ من</h3>
+      </div>
+    </nav>
 </body>
 </html>
